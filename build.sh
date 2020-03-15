15168ec7fa30606c0cef189445d287ed765b2af4
Kunal Sharma <kunal.sharma@firstinsight.com>
2020-03-15 18:39:59 +0530

    created patch build - app version_ 1.0.3

--- build.sh
+++ build.sh
@@ -1,26 +1,28 @@
-function readJson {
-  UNAMESTR=`uname`
+function readJson() {
+  UNAMESTR=$(uname)
   if [[ "$UNAMESTR" == 'Linux' ]]; then
     SED_EXTENDED='-r'
   elif [[ "$UNAMESTR" == 'Darwin' ]]; then
     SED_EXTENDED='-E'
-  fi;
+  fi
 
-  VALUE=`grep -m 1 "\"${2}\"" ${1} | sed ${SED_EXTENDED} 's/^ *//;s/.*: *"//;s/",?//'`
+  VALUE=$(grep -m 1 "\"${2}\"" ${1} | sed ${SED_EXTENDED} 's/^ *//;s/.*: *"//;s/",?//')
 
   if [ ! "$VALUE" ]; then
-    echo "Error: Cannot find \"${2}\" in ${1}" >&2;
-    exit 1;
+    echo "Error: Cannot find \"${2}\" in ${1}" >&2
+    exit 1
   else
-    echo $VALUE ;
-  fi;
+    echo $VALUE
+  fi
 }
 
 cd frontend
-npm run build-prod
-cd ..
-git add -A
-VERSION=`readJson package.json version` || exit 1;
-git commit -m "created $1 build - app version_$VERSION"
+#npm run build-prod
+cd .. || exit 1
+VERSION=$(readJson package.json version) || exit 1
+echo "committing latest changes in $1 build $VERSION"
+git add . -A
+git commit -m "created $1 build - app version_ $VERSION"
+git push origin
 
 npm version $1
